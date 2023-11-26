6746f17523534ab0bb881af6a9c25fceb9feaf78
Christopher Ferris <cferris@google.com>
2023-11-26 17:26:53 +0000

    viva: Enable 32 bit building, but only 64 bit zygote.
    Bug: 229787331
    Test: Build/Boot in progress.
    Change-Id: I4686b567fd96a6cd7c3f6e269f0424811c0b3d6a

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -14,6 +14,9 @@ TARGET_CPU_ABI2 :=
 TARGET_CPU_VARIANT := cortex-a76
 TARGET_CPU_VARIANT_RUNTIME := cortex-a76
 
+# Enable 64-bit for non-zygote.
+ZYGOTE_FORCE_64 := true
+
 # Build the 32 bit targets
 TARGET_2ND_ARCH := arm
 TARGET_2ND_ARCH_VARIANT := armv8-2a

2156b3acd3614ce9ebeeb918521c505ab3c0e3d2
Woomymy <woomy@woomy.be>
2025-03-02 18:08:40 +0100

    viva: Configure Virtual A/B
    Change-Id: Iaab06bd421ea13b359997e57ef13253dcbadceff
    Signed-off-by: Woomymy <woomy@woomy.be>

--- device.mk
+++ device.mk
@@ -4,6 +4,9 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Virtual A/B
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
+
 # Dynamic partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 

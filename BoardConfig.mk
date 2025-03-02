8ce2ba593371986b30385e22ba52e8b6e4ff12d8
Woomymy <woomy@woomy.be>
2025-03-02 17:27:28 +0100

    viva: Define architecture flags
    Change-Id: I1b0356681e53cbfa3ef962ddf1db1abece5f9f1b
    Signed-off-by: Woomymy <woomy@woomy.be>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -6,5 +6,21 @@
 
 DEVICE_PATH := device/xiaomi/viva
 
+# Architecture
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-2a-dotprod
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
+TARGET_CPU_VARIANT := cortex-a76
+TARGET_CPU_VARIANT_RUNTIME := cortex-a76
+
+# Build the 32 bit targets
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv8-2a
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := cortex-a55
+TARGET_2ND_CPU_VARIANT_RUNTIME := cortex-a55
+
 # Inherit from the proprietary version
 include vendor/xiaomi/viva/BoardConfigVendor.mk

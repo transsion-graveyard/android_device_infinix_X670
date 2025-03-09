7b376552938afd060c4580a6db54522834d1ea01
Michael Bestas <mkbestas@lineageos.org>
2025-03-09 12:45:54 +0200

    viva: Shim libneuralnetworks_sl_driver_mtk_prebuilt/libnvram/libsysenv with libbase_shim
    See: https://cs.android.com/android/_/android/platform/system/libbase/+/46fc805282265715266c256fbe34cf1a886c475f
    
    Change-Id: I5127020e18114a13515d9dc382c95d689634243d

--- extract-files.py
+++ extract-files.py
@@ -120,6 +120,14 @@ blob_fixups: blob_fixups_user_type = {
     "vendor/lib64/libmnl.so": blob_fixup()
         .patchelf_version(patchelf_version)
         .add_needed("libcutils.so"),
+    (
+        "vendor/lib/libnvram.so",
+        "vendor/lib64/libnvram.so",
+        "vendor/lib64/libsysenv.so",
+        "vendor/lib64/libtflite_mtk.so",
+        "vendor/bin/hw/android.hardware.neuralnetworks@1.3-service-mtk-neuron"
+    ): blob_fixup()
+        .add_needed('libbase_shim.so'),
 }  # fmt: skip
 
 module = ExtractUtilsModule(

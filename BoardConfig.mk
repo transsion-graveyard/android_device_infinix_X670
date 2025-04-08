03e825e5cfd3d7f5b210dc978fac550b8999db00
Woomymy <woomy@woomy.be>
2025-04-08 23:34:57 +0200

    viva: Build vbmeta with --set_hashtree_disabled_flag
    * Our bootloader is somehow completely broken and crashes if using
      `--flags 3` (and not using exactly the stock avb configuration) (thanks
      MediaTek)
    
    * At least, we can use `--set_hashtree_disabled_flag` (equivalent of
      `--flags 1`) which does exactly what we want to allow flashing Google
      apps
    
    Change-Id: I8bf6e1ae1362803f60ed37ac70f1dc19753c2191
    Signed-off-by: Woomymy <woomy@woomy.be>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -114,6 +114,7 @@ TARGET_VENDOR_PROP += $(CONFIGS_PATH)/props/vendor.prop
 
 # Verified Boot
 BOARD_AVB_ENABLE := true
+BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --set_hashtree_disabled_flag
 BOARD_AVB_ALGORITHM := SHA256_RSA2048
 BOARD_AVB_KEY_PATH := external/avb/test/data/testkey_rsa2048.pem
 

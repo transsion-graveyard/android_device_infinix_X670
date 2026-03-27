a1b4ef62c5a49e76e65e5ab6db9865f831d85299
username <email@example.com>
2026-03-27 14:46:21 +0700

    add axion flags

--- lineage_X670.mk
+++ lineage_X670.mk
@@ -25,3 +25,25 @@ PRODUCT_GMS_CLIENTID_BASE := android-transsion
 PRODUCT_BUILD_PROP_OVERRIDES += \
     DeviceName=X670 \
     BuildFingerprint=Infinix/X670-GL/Infinix-X670:12/SP1A.210812.016/240224V150:user/release-keys
+
+PERF_ANIM_OVERRIDE := true
+WITH_GMS := false
+
+# Time
+LINEAGE_VERSION_APPEND_TIME_OF_DAY := true
+
+# AxionOS
+
+# Define rear camera specs (multiple sensors supported)
+AXION_CAMERA_REAR_INFO := 50MP  # Example: 50MP + 48MP
+
+# Define front camera specs
+AXION_CAMERA_FRONT_INFO := 16MP  # Example: 42MP
+
+# Maintainer name (use "_" for spaces, e.g., "rmp_22" → "rmp 22" in UI)
+AXION_MAINTAINER := R
+
+# Processor name (use "_" for spaces)
+AXION_PROCESSOR := Mediatek_Helio_G96
+
+AXION_DEBUGGING_ENABLED := true

a584e829bb4dda610d7ca08af542e11e07c81ed0
rdndds <rdndds@gmail.com>
2026-04-19 21:40:12 +0700

    try tran audio

--- device.mk
+++ device.mk
@@ -191,6 +191,9 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.device@3.6.vendor:64 \
     android.hardware.camera.provider@2.6.vendor:64
 
+PRODUCT_PACKAGES += \
+    Audioservice.tran \
+
 # Fingerprint
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1-service

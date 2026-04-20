33b0da3dd9e042eba3acbfdfa54009962d740f4e
rdndds <rdndds@gmail.com>
2026-04-21 01:31:28 +0700

    Revert "switch to prebuilt usb"
    This reverts commit a0fbe0cb34f8eafe0fdc92fe1953bc1d22f4983a.

--- device.mk
+++ device.mk
@@ -335,8 +335,11 @@ PRODUCT_COPY_FILES += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.3.vendor \
-    android.hardware.usb.gadget@1.1.vendor
+    android.hardware.usb-service.mediatek \
+    android.hardware.usb.gadget-service.mediatek
+
+# Enable audio accessory support
+$(call soong_config_set,android_hardware_mediatek_usb,audio_accessory_supported,true)
 
 # Vibrator
 PRODUCT_PACKAGES += \

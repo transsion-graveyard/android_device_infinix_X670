1e9b711fe3ddee1e754b992b9427d6763befffd0
rdndds <rdndds@gmail.com>
2026-05-27 00:14:51 +0700

    Revert "try without this patch"
    This reverts commit 2a629bf8614a16f757df0c5724507845e5807863.

--- extract-files.py
+++ extract-files.py
@@ -52,6 +52,8 @@ blob_fixups: blob_fixups_user_type = {
         .add_needed('libjni_shim.so'),
     ('vendor/bin/hw/android.hardware.usb@1.2-service-mediatekv2', 'vendor/lib64/libgoodixhwfingerprint.so', 'vendor/bin/hw/android.hardware.neuralnetworks@1.3-service-mtk-neuron', 'vendor/lib/libnvram.so', 'vendor/lib64/libnvram.so', 'vendor/lib64/libsysenv.so'): blob_fixup()
         .add_needed('libbase_shim.so'),    
+    'vendor/lib64/hw/fingerprint.default.so': blob_fixup()
+        .binary_regex_replace(b'fpsensor_fingerprint', b'fingerprint\x00\x00\x00\x00\x00\x00\x00\x00\x00'),
     "vendor/lib64/hw/audio.primary.mt6781.so": blob_fixup()
         .replace_needed("libutils.so", "libutils-v32.so")
         .replace_needed('libtinyxml2.so', 'libtinyxml2-v34.so')

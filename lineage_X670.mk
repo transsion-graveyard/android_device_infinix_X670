063f99db5cd87852a4d1ddce0cc244d6b3413e21
rdndds <rdndds@gmail.com>
2026-09-14 03:52:23 +0700

    revert infinity

--- lineage_X670.mk
+++ lineage_X670.mk
@@ -12,9 +12,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/infinix/X670/device.mk)
 
 # Inherit some common LineageOS Stuff
-$(call inherit-product, vendor/infinity/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-PRODUCT_NAME := infinity_X670
+PRODUCT_NAME := lineage_X670
 PRODUCT_DEVICE := X670
 PRODUCT_MANUFACTURER := Infinix
 PRODUCT_BRAND := Infinix
